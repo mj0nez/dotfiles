4a83d375f0df944e6ecb7e089749c497c5c79816
Marcel Johannesmann <mj0nez@fn.de>
2023-09-05 04:30:10 +0200

    added postman and dbeaver via snap

---
+++ programs/postman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+# sudo apt install -y tar gzip build-essential
+
+# curl https://dl.pstmn.io/download/latest/linux_64 -O
+
+sudo apt install snapd 
+sudo snap install core postman
