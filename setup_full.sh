46c52cb24145c42ce3702d03a4626c9792b14e2a
Marcel Johannesmann <mj0nez@fn.de>
2023-09-08 17:57:01 +0200

    separated setup scripts

---
+++ setup_full.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# run basic setup
+sudo wget -qO- https://raw.githubusercontent.com/mj0nez/dotfiles/main/setup.sh | bash
+
+# dev-tooling
+bash ./programs/brew.sh
+bash ./programs/snap_apps.sh
+
+
+# Update Ubuntu and get standard repository programs
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
