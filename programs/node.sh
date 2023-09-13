8a1df6213d070ef8848e6bbe48af04c79118b83b
Marcel Johannesmann <mj0nez@fn.de>
2023-09-13 12:49:05 +0200

    fix: load nvm during installation

--- programs/node.sh
+++ programs/node.sh
@@ -3,6 +3,10 @@
 
 PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash'
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 nvm install node  #latest version
 nvm install --lts
 
