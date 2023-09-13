4cba61be0f8c32d911284aab6b1e097654b683a0
Marcel Johannesmann <mj0nez@fn.de>
2023-09-13 12:59:16 +0200

    Update setup_full.sh

--- setup_full.sh
+++ setup_full.sh
@@ -7,6 +7,7 @@ sudo wget -qO- https://raw.githubusercontent.com/mj0nez/dotfiles/main/setup_basi
 # dev-tooling
 bash ./programs/brew.sh
 bash ./programs/snap_apps.sh
+bash ./programs/node.sh
 
 
 # Update Ubuntu and get standard repository programs
