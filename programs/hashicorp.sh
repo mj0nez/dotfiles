b16d780d28e201ca1ef0f2585becb762c8955679
Marcel Johannesmann <mj0nez@fn.de>
2023-11-20 13:49:55 +0100

    feat: added hashicorp deps

--- programs/hashicorp.sh
+++ programs/hashicorp.sh
@@ -9,4 +9,4 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt-get update && sudo apt-get install nomad
+sudo apt-get update && sudo apt-get install nomad consul vagrant
