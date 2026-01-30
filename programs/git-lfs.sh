8d3f60a5a95b90e3bb8b5c7475004f6e4c6772fa
Marcel Johannesmann <mj0nez@proton.me>
2026-01-30 02:19:21 +0100

    feat: added git-lfs

---
+++ programs/git-lfs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+sudo apt install git-lfs
